84bb39fb884ebda3e6481aad1314e720ee4bc438
Bart Massey <bart.massey@gmail.com>
2026-04-22 14:11:24 -0700

    fixed up resampling

--- filter.py
+++ filter.py
@@ -7,11 +7,16 @@ import scipy.signal as signal
 ap = argparse.ArgumentParser()
 ap.add_argument("--order", type=int, default=2)
 ap.add_argument("--cutoff", type=float, default=0.5)
-ap.add_argument("--algorithm", choices=["ma", "firwin"], default="ma")
+ap.add_argument("--algorithm", choices=["ma", "firwin"], default="firwin")
 ap.add_argument("--cascade", type=int, default=1)
 ap.add_argument("--plot", action="store_true")
+ap.add_argument("--downsample", action="store_true")
 ap.add_argument("wav", nargs="?")
 args = ap.parse_args()
+
+if args.downsample:
+    assert args.algorithm == "firwin"
+    args.order = 256
 
 if args.algorithm == "ma":
     order = args.order
@@ -39,15 +44,18 @@ if args.plot:
 # Check for format, for simplicity.
 assert x.dtype == np.int16, "requires 16-bit signed samples"
 # Convert to float samples, single channel, scaled to -1..1.
-x = np.mean(x.astype(np.float32), axis=1) / 32768.0
-
-# Do the convolution explicitly this way. Very slow.
-# y = np.array([1.0 * x[i] - 0.5 * x[i-1] for i in range(1, len(x))], dtype=np.float32)
+x = x.astype(np.float32) / 32768.0
+if x.ndim > 1:
+    x = np.mean(x)
 
 # Do the convolution with numpy. Much faster.
 for _ in range(args.cascade):
     y = np.convolve(x, a)
     x = y
+
+if args.downsample:
+    y = y[::2]
+    fr //= 2
 
 # Convert the output to signed 16-bit integers.
 y = (y * 32767.0).astype(np.int16)
